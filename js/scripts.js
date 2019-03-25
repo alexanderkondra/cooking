b8219ad517a5456007d2c0c0d48f6d0f92934ee8
Alexander Kondra and Evan Hadfield <evan@>
2019-03-25 14:29:19 -0700

    add liter and gallons converter

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+/*var farhenhiet = function(celcius) {
+	return (celcius * 9 / 5) + 32;
+};
+
+var cel = prompt("Enter Celcuios");
+
+alert(farhenhiet(cel));*/
+
+
+
+var liters = function (galons) {
+	return (galons * 3.785);
+};
+var gn = prompt ("Enter Gallons");
+
+alert(liters(gn));
