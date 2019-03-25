bf44a12db13a8ca40b5c1fa48f208cf993de6d72
Alexander Kondra and Evan Hadfield <alexander@>
2019-03-25 14:33:14 -0700

    add onces to grams converter

--- js/scripts.js
+++ js/scripts.js
@@ -7,10 +7,19 @@ var cel = prompt("Enter Celcuios");
 alert(farhenhiet(cel));*/
 
 
-
+// Gallons to Liter converter//
 var liters = function (galons) {
 	return (galons * 3.785);
 };
 var gn = prompt ("Enter Gallons");
 
 alert(liters(gn));
+
+
+//Ounces to Grams converter//
+var grams = function (ounces) {
+	return (ounces * 28.35);
+};
+var oz = prompt ("Enter Ounces");
+
+alert(grams(oz));
